50b7f5099eef37685cfbce5741203d92d3af367d
Felix Becker <felix.b@outlook.com>
2018-08-19 15:16:29 +0200

    Implement logging to PowerShell streams

--- load.ps1
+++ load.ps1
@@ -1,7 +1,13 @@
+param(
+    [ValidateSet('Debug', 'Release')]
+    [string] $Configuration = 'Debug',
+
+    [switch] $NoRestore
+)
 
 Push-Location $PSScriptRoot
 try {
-    ./build.ps1
+    ./build.ps1 -Configuration $Configuration -NoRestore:$NoRestore
     pwsh-preview -NoExit -Command "Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath PSKubectl.psd1)"
 } finally {
     Pop-Location
--- src/CmdletLoggerProvider.cs
+++ src/CmdletLoggerProvider.cs
@@ -1,31 +1,22 @@
 using System;
 using System.Collections.Concurrent;
 using System.Management.Automation;
+using System.Threading;
 using Microsoft.Extensions.Logging;
 
 namespace Kubectl {
     public class CmdletLoggerProvider : ILoggerProvider {
         private readonly Cmdlet cmdlet;
-        private ConcurrentQueue<Action> queue = new ConcurrentQueue<Action>();
 
         public CmdletLoggerProvider(Cmdlet cmdlet) {
             this.cmdlet = cmdlet;
         }
 
         public ILogger CreateLogger(string categoryName) {
-            var logger = new CmdletLogger(this.cmdlet, this.queue);
-            return logger;
+            return new CmdletLogger(cmdlet);
         }
 
-        public void Flush() {
-            foreach (var action in this.queue) {
-                action();
-            }
-        }
-
-        public void Dispose() {
-            this.queue.Clear();
-        }
+        public void Dispose() { }
     }
 }
 
