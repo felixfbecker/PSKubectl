abdfe1c8ec0ca4e198cbd4d8579b1af65ef16f35
Felix Becker <felix.b@outlook.com>
2018-09-02 22:55:41 +0200

    build(load): start in same directory

--- load.ps1
+++ load.ps1
@@ -6,12 +6,5 @@ param(
     [switch] $NoBuild
 )
 
-if (-not $NoBuild) {
-    ./build.ps1 -Configuration $Configuration -NoRestore:$NoRestore
-}
-Push-Location "$PSScriptRoot/src"
-try {
-    pwsh-preview -NoExit -Command "Import-Module -Name '$PSScriptRoot/PSKubectl/PSKubectl.psd1'"
-} finally {
-    Pop-Location
-}
+&"$PSScriptRoot/build.ps1" -Configuration $Configuration -NoRestore:$NoRestore
+pwsh-preview -NoExit -Command "Import-Module -Name '$PSScriptRoot/PSKubectl/PSKubectl.psd1'"
