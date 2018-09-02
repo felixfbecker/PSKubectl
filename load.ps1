25c3920ca4fabd787d2bb9581f9ff26f8c69d07d
Felix Becker <felix.b@outlook.com>
2018-09-02 23:57:55 +0200

    build: fix NoBuild flag

--- load.ps1
+++ load.ps1
@@ -6,5 +6,7 @@ param(
     [switch] $NoBuild
 )
 
-&"$PSScriptRoot/build.ps1" -Configuration $Configuration -NoRestore:$NoRestore
+if (-not $NoBuild) {
+    &"$PSScriptRoot/build.ps1" -Configuration $Configuration -NoRestore:$NoRestore
+}
 pwsh-preview -NoExit -Command "Import-Module -Name '$PSScriptRoot/PSKubectl/PSKubectl.psd1'"
