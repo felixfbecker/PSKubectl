bfab786dbe170a8731caf170f14d9b91bc71482e
Felix Becker <felix.b@outlook.com>
2018-09-03 21:38:58 +0200

    ci: fix path

--- ci/Invoke-Tests.ps1
+++ ci/Invoke-Tests.ps1
@@ -8,7 +8,7 @@ $kubectlProxy = Start-Job { kubectl proxy }
 try {
     $PSBoundParameters['*-Kube*:ApiEndPoint'] = 'http://127.0.0.1:8001'
 
-    Import-Module "$PSScriptRoot/PSKubectl/PSKubectl.psd1"
+    Import-Module "$PSScriptRoot/../PSKubectl/PSKubectl.psd1"
     Invoke-Pester "$PSScriptRoot/../Tests"
 } finally {
     Stop-Job -Job $kubectlProxy
