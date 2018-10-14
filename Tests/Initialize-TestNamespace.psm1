d0302a6f5c45faef4ef45f2b10ecd58ef839242c
Felix Becker <felix.b@outlook.com>
2018-10-14 21:38:56 +0200

    test: add safety check to prevent messing with production clusters

--- Tests/Initialize-TestNamespace.psm1
+++ Tests/Initialize-TestNamespace.psm1
@@ -2,6 +2,10 @@ Import-Module "$PSScriptRoot/Streams.psm1"
 Import-Module "$PSScriptRoot/Invoke-Executable.psm1"
 
 function Initialize-TestNamespace {
+    $context = Invoke-Executable { kubectl config current-context }
+    if ($context -ne 'docker-for-desktop' -and $context -ne 'minikube') {
+        throw "Kube context is '$context', expected 'docker-for-desktop' or 'minikube'. Aborting for safety reasons."
+    }
     $namespace = 'pskubectltest'
     Write-Information 'Setting up Kubernetes cluster'
     Invoke-Executable { kubectl apply -f $PSScriptRoot/test.Namespace.yml } | Out-Stream -SuccessTarget 6
